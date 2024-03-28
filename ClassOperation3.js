84a190c9232827a8f0719e17067c18c7a67ebe60
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 11:58:25 +0100

    js

---
+++ ClassOperation3.js
@@ -0,0 +1,17 @@
+let person = {
+    firstname:"John",
+    lastname:"Doe",
+    age:30,
+    gender:"Male"
+}
+
+console.log(person.age);
+
+let array = [];
+//let arr = new Array(3);
+array[0] = "Manner"
+array[1] = "code"
+array[2] = 23
+array[3] = null
+array[4] = 2
+console.log(array);
