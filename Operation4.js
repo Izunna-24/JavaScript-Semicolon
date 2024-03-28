6ac5449f4ab16d62b5504d88bc99d5ee1ac563a3
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 12:17:44 +0100

    js

---
+++ Operation4.js
@@ -0,0 +1,18 @@
+let update = {
+    id :1,
+    available:true,
+    count: 2,
+    name:"Izunna",
+    author:"Izunna"
+}
+
+console.log(update);
+
+// var ans = new Ans();
+// ans.id = 1;
+// ans.available = false;
+// ans.couunt = 4;
+// ans.name = "Person";
+// ans.author = "Angel";
+
+// console.log(ans);
