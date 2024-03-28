13f180b0977314b5c43c691f5934789755ac6796
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 11:01:16 +0100

    okay

---
+++ ClassOperation2.js
@@ -0,0 +1,29 @@
+ x = 2;
+ y = 2;
+sum = (x + y);
+twicesum = 2*sum;
+
+console.log("twicesum =", twicesum);
+
+number = 8;
+remainder = (8 % 10) + 8;
+console.log("remainder is", remainder);
+
+x = 2;
+n = 3;
+multiply = (x * x)**3;
+console.log("multiplication = ", multiply);
+
+a = 2;
+x = 3
+y = 4
+z = 5
+product = (x*y*z);
+divide = a/product;
+console.log("divide =", divide)
+x = a + 10;
+console.log("x = ", x);
+y = 10 - a;
+console.log("y = ", y);
+z = a % 10; 
+console.log("z = ", z);
---
+++ ClassTasks.js
@@ -0,0 +1,16 @@
+var1 = 2;
+var2 = var1;
+var3 = var2;
+ 
+console.log("\n var1 = " + var1 +" \n var2 = " + var2 +"\n var3 = " +var3);
+
+var1 = var2;
+ var2 = var3;
+ var3 = var1;
+
+ console.log("\n var1 = " + var1 +" \n var2 = " + var2 +"\n var3 = " +var3);
+
+ var number = 5;
+ console.log(number);
+ var number = 7;
+  console.log(number);
