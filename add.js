4bd0da2fdeb7808975a29c2e2d1b2949e073bb1c
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-18 14:10:05 +0100

    javascript

--- add.js
+++
@@ -1,8 +0,0 @@
-function addNumbers(x,y){
-    let answer = x + y;
-    return answer
-}
-
-addNumbers(2,3)
-
-module.exports = addNumbers;
--- jestFunctionsTest.test.js
+++ jestFunctionsTest.test.js
@@ -1,4 +1,4 @@
-let addNumbers = require("./add.js")//require is used to assign a file to a variable
+let {addNumbers, evenNumbers } = require("./jestFunctions.js")//require is used to assign a file to a variable
 
 test("Add two numbers",() =>{  // test takes in description, call back function, the test body: arrange = given, act = when,assert
 let a = 3;// arrange
@@ -6,4 +6,11 @@ let b = 5;// arrange
 let answer = addNumbers(a,b);//act,when
 
 expect(answer).toBe(8);//assert
+})
+
+test("filter even numbbers", () =>{
+let arr = [1,2,3,4,5,6];
+let answer = evenNumbers(arr);
+expect(answer).toEqual([2,4,6])
+
 })
